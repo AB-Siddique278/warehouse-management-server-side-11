6733f0f8a73b52824ef8292a20ccedc64cfba563
AB-Siddique278 <absiddique1060@gmail.com>
2022-04-29 12:54:25 +0600

    connect with server

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const { request } = require('express');
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
-const port =process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 //middleware
@@ -10,6 +11,31 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/',(req, res)=>{
-    res.send('Running Genius Server')
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a3mzi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log("Fruits Basket Connected");
+    // perform actions on the collection object
+    client.close();
 });
+
+
+
+app.get('/', (req, res) => {
+    res.send('Running Fruits Basket Server')
+});
+
+app.listen(port, () => {
+    console.log('Leasing to port', port);
+})
+
+//fruitsBasket
+//9sj6QIJtdgtHqsxg
+
+
+
+
+//fruits_Basket
+//M6e2Skc597UqEoGY
