94021f85fc42733d8cee358c578722d97a7bc907
AB-Siddique278 <absiddique1060@gmail.com>
2022-05-02 23:49:40 +0600

    create my Item page

--- index.js
+++ index.js
@@ -23,46 +23,35 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
-async function run(){
-    try{
+async function run() {
+    try {
 
         await client.connect();
         const serviceCollection = client.db('fruits').collection('service');
-       app.get('/service', async (req, res) =>{
-        const query = {};
-        const cursor =serviceCollection.find(query);
-        const service = await cursor.toArray();
-        res.send(service);
+        app.get('/service', async (req, res) => {
 
-       });
-       app.get('/service:id', async(req, res)=>{
-           const id = req.params.id;
-           const query={_id: ObjectId(id)};
-           const service =await serviceCollection.findOne(query);
-           res.send(service);
-       });
-       //post items
-       app.post('/service', async(req, res) =>{
-           const newSevice =req.body;
-           const result =await serviceCollection.insertOne(newSevice)
-           res.send(result);
-       });
+            const email = req.query?.email;
+            const query = { email: email };
 
-       //update items
-       
-       app.put('/service:id', async(req, res) =>{
-           const id = req.params.id;
-           const updateItem =req.body;
-           const filter ={_id: ObjectId(id)};
-           const options= {upsert: true };
-           const updatedDoc = {
-               $set:{
-                quantity:updateItem.quantity
-               }
-           };
-           const result =await serviceCollection.updateOne(filter, updatedDoc, options);
-           res.send(result);
-       })
+            // const query = {};
+            const cursor = serviceCollection.find(query);
+            const service = await cursor.toArray();
+            res.send(service);
+
+        });
+
+
+        // app.get('/service', async (req, res) => {
+        //     const email = req.query?.email;
+
+        //     const query = { email: email };
+
+
+        //     const cursor = serviceCollection.find(query);
+        //     const service = await cursor.toArray();
+        //     res.send(service);
+
+        // });
 
 
 
@@ -71,21 +60,65 @@ async function run(){
 
 
 
+        app.get('/service:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+        //post items
+        app.post('/service', async (req, res) => {
+            const newSevice = req.body;
+            const result = await serviceCollection.insertOne(newSevice)
+            res.send(result);
+        });
+
+        //update items
+
+        app.put('/service:id', async (req, res) => {
+            const id = req.params.id;
+            const updateItem = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: updateItem.quantity
+                }
+            };
+            const result = await serviceCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
+
+        //items
+        // app.get('/service', async(req, res) =>{
+        //     const email = req.query.email;
+
+        //     const query = {email: email};
+        //     const cursor = serviceCollection.find(query);
+        //     const myItems = await cursor.toArray();
+        //     req.send(myItems)
+        // })
 
 
 
 
-       //Delete items
-       app.delete('/service/:id', async(req, res) =>{
-           const id = req.params.id;
-           const query={_id: ObjectId(id)};
-           const result = await serviceCollection.deleteOne(query);
-           res.send(result);
 
-       });
+
+
+
+
+        //Delete items
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+
+        });
 
     }
-    finally{
+    finally {
 
     }
 
@@ -109,7 +142,7 @@ app.get('/', (req, res) => {
     res.send('Running Fruits Basket Server')
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('Leasing to port', port);
 })
 
