6927f51f35de64c254d21f52f8c003584e85484b
AB-Siddique278 <absiddique1060@gmail.com>
2022-05-08 08:37:47 +0600

    edits

--- index.js
+++ index.js
@@ -13,34 +13,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
-
-
-
-
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6hfog.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
-
-
-
-
 
 async function run() {
     try {
 
         await client.connect();
         const serviceCollection = client.db('fruits').collection('service');
-        
-
 
         app.get('/service', async (req, res) => {
 
@@ -55,8 +35,6 @@ async function run() {
         });
 
 
-
-
         app.get('/allitems', async (req, res) => {
             // const email = req.query?.email;
 
@@ -69,12 +47,6 @@ async function run() {
             res.send(result);
 
         });
-
-
-
-
-
-
 
 
         app.get('/service:id', async (req, res) => {
@@ -107,24 +79,6 @@ async function run() {
             const result = await serviceCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
         })
-
-
-        //items
-        // app.get('/service', async(req, res) =>{
-        //     const email = req.query.email;
-
-        //     const query = {email: email};
-        //     const cursor = serviceCollection.find(query);
-        //     const myItems = await cursor.toArray();
-        //     req.send(myItems)
-        // })
-
-
-
-
-
-
-
 
 
         //Delete items
@@ -172,34 +126,14 @@ async function run() {
         })
 
 
-
-
-
-
-
-
-
-
     }
     finally {
 
     }
 
 
-
 }
 run().catch(console.dir);
-
-
-
-
-
-
-
-
-
-
-
 
 app.get('/', (req, res) => {
     res.send('Running Fruits Basket Server')
@@ -208,25 +142,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Leasing to port', port);
 })
-
-//fruitsBasket
-//9sj6QIJtdgtHqsxg
-
-
-
-
-//fruits_Basket
-//M6e2Skc597UqEoGY
-
-//fruits
-//QdvRMqzLa3doNM5B
-
-//DB_USER=fruits
-//DB_PASS=QdvRMqzLa3doNM5B
-
-
-
-
-
-
-//'2b6b9b35c7abfd465f4d03eb978976b09790ac8337cf5f42a1b7a78f69b37f09b4682464b7d081d27842f6cd1a4c76a914e3f5f3257ded94ebde12715083ec11'
